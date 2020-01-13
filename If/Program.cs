a6e2e4532449c137210a5eb87a875abce70b7105
Raúl López <raul.lopez@ingenoba.com>
2020-01-13 11:07:55 +0100

    Add project files.

---
+++ If/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Variables boleanas
+            //bool mayorDeEdad= false;
+            //Console.WriteLine(mayorDeEdad);
+            //Console.ReadLine();
+
+            //// !Not
+
+            //mayorDeEdad = !false;
+            //Console.WriteLine(mayorDeEdad);
+            //Console.ReadLine();
+
+            ////Operadores relacionales
+
+            ////Operador de Igualdad
+
+            //bool esIgual = 42 == 42; // Aqui estamos preguntando si 42 es igual a 42 . Y devolveremos true en este caso
+            //bool esIgual1 = 42 == 43; // Estamos preguntado si 42 es igual a 43 . Entonces sera falso.
+            //bool esIgual2 = 42 != 42; // Aqui estamos preguntando si 42 no es igual a 42 . Y devolveremos true en este caso
+            //Console.WriteLine(esIgual);
+            //Console.WriteLine(esIgual1);
+            //Console.WriteLine(esIgual2);
+
+            //// Tambien podemos aplicar la logica con variables en vez de valores especificos
+            //Console.ReadLine();
+            //int numero1 = 100;
+            //int numero2 = 100;
+            //bool resultado = numero1 == numero2;
+            //Console.WriteLine(resultado);
+
+
+            //if (expresion booleana)
+            // expresion booleana -1
+            //else 
+            //enunciado2
+
+            // ELSE - IF 
+
+            //int dia = 3;
+
+            //if (dia == 0)
+            //{
+            //    Console.WriteLine("Lunes");
+            //}
+
+            //else if(dia == 1)
+            //{
+            //    Console.WriteLine("Martes");
+            //}
+
+            //else if (dia == 2)
+            //{
+            //    Console.WriteLine("Miercoles");
+            //}
+
+            //else if (dia == 3)
+            //{
+            //    Console.WriteLine("Jueves");
+            //}
+
+            //else if (dia == 4)
+            //{
+            //    Console.WriteLine("Viernes");
+            //}
+
+            //else if (dia == 5)
+            //{
+            //    Console.WriteLine("Sabado");
+            //}
+
+            //else if (dia == 6)
+            //{
+            //    Console.WriteLine("Domingo");
+            //}
+
+
+            //Console.WriteLine("Introduce un numero para averiguar el dia de la semana");
+
+            //int dia = Convert.ToInt32(Console.ReadLine());
+
+            //switch (dia)
+
+            //{
+
+            //    case 0: Console.WriteLine("Lunes");
+            //            break;
+
+            //    case 1:
+            //         Console.WriteLine("Martes");
+            //         break;
+
+            //    case 2:
+            //        Console.WriteLine("Miercoles");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("Jueves");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Viernes");
+            //        break;
+
+            //    case 5:
+            //        Console.WriteLine("Sabado");
+            //        break;
+
+            //    case 6:
+            //        Console.WriteLine("Domingo");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Valor Incorrecto");
+            //        break;
+
+
+            }
+
+
+
+        }
+    }
+}
